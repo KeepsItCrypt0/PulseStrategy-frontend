96f6cdd30a3b7e71a555073a324c0e6373e4ff16
PulseStrategy <PLSstrategy@gmail.com>
2025-06-14 03:38:43 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        'custom-red': {
-          600: '#8B0000', // Dark red from logo
-          700: '#A52A2A', // Darker red for hover
+        purple: {
+          600: '#6B43C7',
+          700: '#5A38A6',
         },
-        'custom-blue': {
-          600: '#191970', // Midnight blue from logo
-          700: '#1E1A7B', // Darker blue for hover
-        },
-        'custom-purple': {
-          600: '#4B0082', // Indigo from logo
-          700: '#5D0E8B', // Darker purple for hover
-        },
-        'custom-gray': {
-          400: '#D3D3D3', // Light gray for disabled
-          500: '#A9A9A9', // Medium gray for disabled
-          600: '#8A8A8A', // Dark gray for disabled
+        pink: {
+          500: '#FF69B4',
+          600: '#DB4E93',
         },
       },
       fontFamily: {
